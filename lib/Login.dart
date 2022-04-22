9579efb6f4b1b625d350cd4e58c0884a0e83f18d
alfarizi28 <alfarizibonex99@gmail.com>
2022-04-23 01:24:05 +0800

    aaa

--- lib/Login.dart
+++ lib/Login.dart
@@ -14,9 +14,53 @@ class Login extends StatelessWidget {
         children: [
           Container(
             width: mediaQueryWidth * 10,
-            height: mediaQueryHeight * 0.5,
+            height: mediaQueryHeight * 0.3,
             child: const Image(
-                fit: BoxFit.cover, image: AssetImage("images/Login.png")),
+                fit: BoxFit.cover, image: AssetImage("images/Logo.png")),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: mediaQueryWidth * 0.8,
+              height: mediaQueryHeight * 0.03,
+              // color: Colors.amber,
+              child: const Text(
+                "LOG IN",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            width: mediaQueryWidth * 0.8,
+            height: mediaQueryHeight * 0.05,
+            child: const Text(
+                "Selamat datang kembali, silahkan isi username dan password dengan benar",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14,
+                )),
+          ),
+          Container(
+            width: mediaQueryWidth * 0.8,
+            height: mediaQueryHeight * 0.06,
+            // color: Colors.amber,
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  hintText: "Masukan Email",
+                  prefixIcon: Icon(
+                    Icons.person,
+                    size: 28,
+                    color: Colors.black,
+                  ),
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black))),
+            ),
           ),
         ],
       ),
